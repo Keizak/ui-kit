23e342b28236c523bac1b5dc6bf3292d16292d70
keizak <keizak@yandex.ru>
2022-10-11 17:24:33 +0300

    OPTIMIZE

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,26 +1,35 @@
-const alias = require("rollup-plugin-alias");
+const alias = require('rollup-plugin-alias');
+// const { visualizer } = require('rollup-plugin-visualizer');
 
 const extensions = ['.js', '.ts', '.jsx', '.tsx'];
 
-
 module.exports = {
-    rollup(config, options) {
-        config.output =  {
-            dir: 'dist',
-                format: 'esm',
-                preserveModules: true,
-                preserveModulesRoot: 'src',
-                sourcemap: true,
-        }
-        config.plugins.push(
-            alias({
-                resolve: extensions,
-                entries: [
-                    {find: 'react', replacement: './node_modules/react'},
-                    {find: 'react-dom', replacement: './node_modules/react-dom'},
-                ]
-            }),
-        );
-        return config;
-    },
+  rollup(config) {
+    config.output = {
+      dir: 'dist',
+      format: 'esm',
+      preserveModules: false,
+      sourcemap: false,
+      tsconfig: 'tsconfig.json',
+      minify: true,
+      writeMeta: false,
+      transpileOnly: true,
+    };
+    // config.plugins.push(
+    //   visualizer({
+    //     filename: 'stats.html',
+    //   })
+    // );
+    config.plugins.push(
+      alias({
+        resolve: extensions,
+        entries: [
+          { find: 'react', replacement: './node_modules/react' },
+          { find: 'react-dom', replacement: './node_modules/react-dom' },
+        ],
+      })
+    );
+
+    return config;
+  },
 };
