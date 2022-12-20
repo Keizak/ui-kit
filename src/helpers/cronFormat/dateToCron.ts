30cc98b075d1b05d176090d5170ab4af864449ee
Liza <elizabethspivak99@gmail.com>
2022-12-20 14:40:02 +0300

    Cron fix (#19)
    * fix cron pattern
    
    * fix cron pattern
    
    * fix cron pattern
    
    * fix cron pattern
    
    * fix cron pattern
    
    * fix cron pattern

--- src/helpers/cronFormat/dateToCron.ts
+++ src/helpers/cronFormat/dateToCron.ts
@@ -1,20 +1,36 @@
 import dayjs from 'dayjs';
 
+
+enum MonthEnum {
+    JAN = 1,
+    FEB = 2,
+    MAR = 3,
+    APR = 4,
+    MAY = 5,
+    JUN = 6,
+    JUL = 7,
+    AUG = 8,
+    SEP = 9,
+    OCT = 10,
+    NOV = 11,
+    DEC = 12
+}
+
 /**
  * Функция приобразующая формат Даты в крон формат
  */
 export const dateToCron = (date: Date): string => {
-  /**
-   * Преобразуем значения из даты в отдельные числа
-   */
-  const minutes = dayjs(date).get('m');
-  const hours = dayjs(date).get('h');
-  const days = dayjs(date).get('D');
-  const months = +dayjs(date).get('M') + 1;
-  const dayOfWeek = dayjs(date).day();
+    /**
+     * Преобразуем значения из даты в отдельные числа
+     */
+    const minutes = dayjs(date).get('m');
+    const hours = dayjs(date).get('h');
+    const days = dayjs(date).get('D');
+    const months: number = +dayjs(date).get('M') + 1;
+    const dayOfWeek = dayjs(date).day();
 
-  /**
-   * Формируем из отдельных значения крон выражения формата "m h d m dw"
-   */
-  return `* ${minutes} ${hours} ${days} ${months} ${dayOfWeek} *`;
+    /**
+     * Формируем из отдельных значения крон выражения формата "m h d m dw"
+     */
+    return `0 ${minutes} ${hours} ${days} ${MonthEnum[months]} ${dayOfWeek} *`;
 };
