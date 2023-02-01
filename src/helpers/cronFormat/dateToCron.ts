96036c6543c29a39d67f791b951945a7d05d6b75
Keizak <Keizak@yandex.ru>
2023-02-02 00:32:30 +0300

    eslint

--- src/helpers/cronFormat/dateToCron.ts
+++ src/helpers/cronFormat/dateToCron.ts
@@ -1,45 +1,44 @@
 import dayjs from 'dayjs';
 
-
 export enum MonthEnum {
-    JAN = 1,
-    FEB = 2,
-    MAR = 3,
-    APR = 4,
-    MAY = 5,
-    JUN = 6,
-    JUL = 7,
-    AUG = 8,
-    SEP = 9,
-    OCT = 10,
-    NOV = 11,
-    DEC = 12
+  JAN = 1,
+  FEB = 2,
+  MAR = 3,
+  APR = 4,
+  MAY = 5,
+  JUN = 6,
+  JUL = 7,
+  AUG = 8,
+  SEP = 9,
+  OCT = 10,
+  NOV = 11,
+  DEC = 12,
 }
 
 export enum WeekEnum {
-    MON = 1,
-    TUE = 2,
-    WED = 3,
-    THU = 4,
-    FRI = 5,
-    SAT = 6,
-    SUN = 7
+  MON = 1,
+  TUE = 2,
+  WED = 3,
+  THU = 4,
+  FRI = 5,
+  SAT = 6,
+  SUN = 7,
 }
 
 /**
  * Функция приобразующая формат Даты в крон формат
  */
 export const dateToCron = (date: Date): string => {
-    /**
-     * Преобразуем значения из даты в отдельные числа
-     */
-    const minutes = dayjs(date).get('m');
-    const hours = dayjs(date).get('h');
-    const days = dayjs(date).get('D');
-    const months: number = +dayjs(date).get('M') + 1;
+  /**
+   * Преобразуем значения из даты в отдельные числа
+   */
+  const minutes = dayjs(date).get('m');
+  const hours = dayjs(date).get('h');
+  const days = dayjs(date).get('D');
+  const months: number = +dayjs(date).get('M') + 1;
 
-    /**
-     * Формируем из отдельных значения крон выражения формата "m h d m dw"
-     */
-    return `0 ${minutes} ${hours} ${days} ${MonthEnum[months]} ? *`;
+  /**
+   * Формируем из отдельных значения крон выражения формата "m h d m dw"
+   */
+  return `0 ${minutes} ${hours} ${days} ${MonthEnum[months]} ? *`;
 };
--- src/ui-components/BasicSelect/BasicSelect.tsx
+++ src/ui-components/BasicSelect/BasicSelect.tsx
@@ -1,153 +1,170 @@
+import React, { useState } from 'react';
+
+import { Block } from '@mui/icons-material';
 import {
-    Box,
-    createTheme,
-    FormControl, MenuItem, OutlinedInput,
-    SelectChangeEvent,
-    Theme,
-    ThemeProvider
-} from "@mui/material";
-import React, {useState} from "react";
-import {DefaultTheme, SxProps} from "@mui/system";
-import {nanoid} from "nanoid";
-import {Block} from "@mui/icons-material";
-import {StyledKeyboardArrowDownIcon, StyledSelect} from "../../ui-styled-components/common";
+  Box,
+  createTheme,
+  FormControl,
+  MenuItem,
+  OutlinedInput,
+  SelectChangeEvent,
+  Theme,
+  ThemeProvider,
+} from '@mui/material';
+import { DefaultTheme, SxProps } from '@mui/system';
+import { nanoid } from 'nanoid';
+
+import {
+  StyledKeyboardArrowDownIcon,
+  StyledSelect,
+} from '../../ui-styled-components/common';
 
 export type BasicSelectProps = {
-    label: string;
-    options: OptionType[];
-    onSelect: (value: any) => void;
-    minWidth?: number | string;
-    size?: 'small' | 'medium' | undefined;
-    opacityText?: number;
-    margin?: number | string;
-    height?: number | string;
-    colorIcon?: string;
-    colorText?: string;
-    sx?: SxProps<Theme>;
-    value?: string | string[] | number[] | null;
-    defaultValue?: string | string[] | null;
-    menuType?: 'vertical' | 'horizontal';
-    menuItemWidth?: string;
-    theme?: DefaultTheme;
-    nullableTitle?: string
-    addNullableValue?: boolean
+  label: string;
+  options: OptionType[];
+  onSelect: (value: any) => void;
+  minWidth?: number | string;
+  size?: 'small' | 'medium' | undefined;
+  opacityText?: number;
+  margin?: number | string;
+  height?: number | string;
+  colorIcon?: string;
+  colorText?: string;
+  sx?: SxProps<Theme>;
+  value?: string | string[] | number[] | null;
+  defaultValue?: string | string[] | null;
+  menuType?: 'vertical' | 'horizontal';
+  menuItemWidth?: string;
+  theme?: DefaultTheme;
+  nullableTitle?: string;
+  addNullableValue?: boolean;
 };
 
 type OptionType = {
-    title: string
-    value: string | number
-}
+  title: string;
+  value: string | number;
+};
 
-export const NullString = '___65DYD3DGQWDG__'
+export const NullString = '___65DYD3DGQWDG__';
 
 export const BasicSelect: React.FC<BasicSelectProps> = ({
-                                                            theme = createTheme({}),
-                                                            onSelect,
-                                                            value = null,
-                                                            defaultValue = null,
-                                                            height = '36px',
-                                                            margin,
-                                                            sx,
-                                                            menuType = 'vertical',
-                                                            menuItemWidth = '50px',
-                                                            minWidth = 224,
-                                                            size = 'small',
-                                                            colorIcon,
-                                                            options,
-                                                            label,
-                                                            opacityText, colorText,
-                                                            nullableTitle = 'Not selected',
-                                                            addNullableValue = false
-                                                        }) => {
+  theme = createTheme({}),
+  onSelect,
+  value = null,
+  defaultValue = null,
+  height = '36px',
+  margin,
+  sx,
+  menuType = 'vertical',
+  menuItemWidth = '50px',
+  minWidth = 224,
+  size = 'small',
+  colorIcon,
+  options,
+  label,
+  opacityText,
+  colorText,
+  nullableTitle = 'Not selected',
+  addNullableValue = false,
+}) => {
+  const parsedValue =
+    // eslint-disable-next-line no-nested-ternary
+    !value && !addNullableValue
+      ? label
+      : !value && addNullableValue
+      ? NullString
+      : value;
 
-    const parsedValue = !value && !addNullableValue ? label : !value && addNullableValue ? NullString : value
+  const [selectValue, setSelectValue] = useState<
+    string | string[] | number[] | null
+  >(parsedValue);
 
-    const [selectValue, setSelectValue] = useState<string | string[] | number[] | null>(
-        parsedValue
-    );
+  let nullableItem = { value: NullString, title: nullableTitle };
 
-    let nullableItem = {value: NullString, title: nullableTitle}
+  defaultValue = defaultValue === null ? NullString : defaultValue;
 
-    defaultValue = defaultValue === null ? NullString : defaultValue
+  //---------------------------------------------Дополнительные функции---------------------------------------------
+  /**
+   * Функция обработчик выбора значения
+   */
+  const handleChange = (event: SelectChangeEvent<any>) => {
+    let value = event.target.value;
 
-    //---------------------------------------------Дополнительные функции---------------------------------------------
-    /**
-     * Функция обработчик выбора значения
-     */
-    const handleChange = (event: SelectChangeEvent<any>) => {
-        let value = event.target.value;
+    if (!onSelect) return;
 
-        if (!onSelect) return
+    if (value === NullString) {
+      onSelect(null);
+      setSelectValue(NullString);
+    } else {
+      onSelect(value);
+      setSelectValue(value);
+    }
+  };
 
-        if (value === NullString) {
-            onSelect(null)
-            setSelectValue(NullString)
-        } else {
-            onSelect(value)
-            setSelectValue(value)
-        }
-    };
+  //-----------------------------------------------JSX-----------------------------------------------
 
-    //-----------------------------------------------JSX-----------------------------------------------
-
-
-    return (
-        <ThemeProvider theme={theme}>
-            <Box sx={{minWidth: minWidth, margin: margin}}>
-                <FormControl fullWidth sx={{height: height, ...sx}}>
-                    <StyledSelect
-                        displayEmpty
-                        value={selectValue}
-                        defaultValue={defaultValue}
-                        onChange={handleChange}
-                        input={<OutlinedInput/>}
-                        size={size}
-                        renderValue={(value) =>
-                            value ? (
-                                <span
-                                    style={{
-                                        opacity: opacityText,
-                                        color: colorText,
-                                    }}
-                                >
-                            {selectValue === NullString ? nullableItem.title : selectValue}
-                                </span>
-                            ) : (
-                                <em
-                                    style={{
-                                        opacity: opacityText,
-                                        color: colorText,
-                                    }}
-                                >
-                                    {label}
-                                </em>
-                            )
-                        }
-                        IconComponent={(classes) => {
-                            return (
-                                <StyledKeyboardArrowDownIcon
-                                    className={classes.className}
-                                    sx={{color: colorIcon}}
-                                />
-                            );
-                        }}
-                    >
-                        {addNullableValue && <MenuItem
-                            value={nullableItem.value}>{nullableItem.title}</MenuItem>}
-                        {options.map((option) => (
-                            <MenuItem key={nanoid()} value={option.value}>
-                                {menuType === 'vertical' ? (
-                                    option.title
-                                ) : (
-                                    <Block name={'item'} width={menuItemWidth} key={nanoid()}>
-                                        {option.title}
-                                    </Block>
-                                )}
-                            </MenuItem>
-                        ))}
-                    </StyledSelect>
-                </FormControl>
-            </Box>
-        </ThemeProvider>
-    )
+  return (
+    <ThemeProvider theme={theme}>
+      <Box sx={{ minWidth: minWidth, margin: margin }}>
+        <FormControl fullWidth sx={{ height: height, ...sx }}>
+          <StyledSelect
+            displayEmpty
+            value={selectValue}
+            defaultValue={defaultValue}
+            onChange={handleChange}
+            input={<OutlinedInput />}
+            size={size}
+            renderValue={(value) =>
+              value ? (
+                <span
+                  style={{
+                    opacity: opacityText,
+                    color: colorText,
+                  }}
+                >
+                  {selectValue === NullString
+                    ? nullableItem.title
+                    : selectValue}
+                </span>
+              ) : (
+                <em
+                  style={{
+                    opacity: opacityText,
+                    color: colorText,
+                  }}
+                >
+                  {label}
+                </em>
+              )
+            }
+            IconComponent={(classes) => {
+              return (
+                <StyledKeyboardArrowDownIcon
+                  className={classes.className}
+                  sx={{ color: colorIcon }}
+                />
+              );
+            }}
+          >
+            {addNullableValue && (
+              <MenuItem value={nullableItem.value}>
+                {nullableItem.title}
+              </MenuItem>
+            )}
+            {options.map((option) => (
+              <MenuItem key={nanoid()} value={option.value}>
+                {menuType === 'vertical' ? (
+                  option.title
+                ) : (
+                  <Block name={'item'} width={menuItemWidth} key={nanoid()}>
+                    {option.title}
+                  </Block>
+                )}
+              </MenuItem>
+            ))}
+          </StyledSelect>
+        </FormControl>
+      </Box>
+    </ThemeProvider>
+  );
 };
