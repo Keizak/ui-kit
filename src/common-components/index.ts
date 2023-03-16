bfdff3c1f13995bcbe7c87946f42cdfd0878443f
Keizak <Keizak@yandex.ru>
2023-03-16 19:55:41 +0300

    add import type

--- src/common-components/index.ts
+++ src/common-components/index.ts
@@ -1,9 +1,12 @@
 import { ButtonRequest } from './ButtonRequest/buttonRequest';
-import { StreamStatusType } from './StartEntityWithStreamButton/api/api';
+import {
+  StreamStatusType,
+  StreamTypes,
+} from './StartEntityWithStreamButton/api/api';
 import {
   createStreamButtonPropsType,
   StartEntityWithStreamButton,
 } from './StartEntityWithStreamButton/createStreamButton';
 
-export type { createStreamButtonPropsType, StreamStatusType };
+export type { createStreamButtonPropsType, StreamStatusType, StreamTypes };
 export { StartEntityWithStreamButton, ButtonRequest };
