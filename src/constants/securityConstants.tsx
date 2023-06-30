135423fa77cdbcc695fb4c069b58f106013076de
Liza <elizabethspivak99@gmail.com>
2023-06-30 17:05:42 +0300

    fix securityConstants (#105)

--- src/constants/securityConstants.tsx
+++ src/constants/securityConstants.tsx
@@ -1,10 +1,10 @@
 export const securityConstants = {
   apiBaseUrl:
     process.env.REACT_APP_API_BASE_URL ||
-    'https://labs-api.staging.it-kamasutra.com/api/',
+    'https://crm.staging.it-incubator.ru/api/',
   botUrl:
     process.env.REACT_APP_BOT_URL ||
-    'https://st-students-bot-api.herokuapp.com',
+    'https://students-bot-api.staging.it-incubator.ru',
   personalPageBaseUrl:
     process.env.REACT_APP_PERSONAL_PAGE_API_URL ||
     'https://personal-page.staging.it-incubator.ru/api/3.0',
